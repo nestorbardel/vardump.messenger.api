8b9c7ba4bb3ecb3b203827a97857f7d1445e4404
nestorbardel <nestorbardel@gmail.com>
2025-09-08 15:25:04 -0300

    feat: add CORS support and update package.json dependencies

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,26 @@ import { Express } from 'express';
 import { NodemailerAdapter } from './infrastructure/adapters/NodemailerAdapter';
 import { SendEmailUseCase } from './core/usecases/SendEmailUseCase';
 import { EmailController } from './infrastructure/controllers/EmailController';
-
 const app: Express = express();
+const cors = require('cors');
+const allowedOrigins = [
+  'https://www.vardump.com.ar',
+  'https://vardump.com.ar',
+  'http://localhost:4200' // Para desarrollo
+];
+
+app.use(cors({
+  origin: function (origin: string | undefined, callback: (error: Error | null, allow?: boolean) => void) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+
 app.use(express.json());
 
 const  emailSender = new NodemailerAdapter();
